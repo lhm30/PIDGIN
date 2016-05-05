66b422ad55c01f31900515b085fbe58aff047b0f
lhm30 <lhm30@cam.ac.uk>
2016-05-05 13:08:52 +0100

    update ad

--- ad_analysis.py
+++ ad_analysis.py
@@ -9,31 +9,44 @@ import sys
 from pylab import save
 import numpy as np
 import pymysql
+import math
 import getpass
 from multiprocessing import Pool
 import multiprocessing
 import getpass
 multiprocessing.freeze_support()
-N_cores = 3
+N_cores = 25
+
+def introMessage():
+	print '=============================================================================================='
+	print ' Author: Lewis Mervin\n Email:  lhm30@cam.ac.uk\n Supervisor: Dr. A. Bender'
+	print ' Address: Centre For Molecular Informatics, Dept. Chemistry, Lensfield Road, Cambridge CB2 1EW'
+	print '==============================================================================================\n'
+	return 
 
 def login():
     user = raw_input("Enter Username for PIDGIN DB [%s]: " % getpass.getuser())
     if not user:
         user = getpass.getuser()
-
     pprompt = lambda: (getpass.getpass(), getpass.getpass('Retype password: '))
-
     p1, p2 = pprompt()
     while p1 != p2:
         print('Passwords do not match. Try again')
         p1, p2 = pprompt()
-
     return user, p1
 	
 def calcNormalFingerprints(smiles):
 	m1 = Chem.MolFromSmiles(smiles)
 	fp = AllChem.GetMorganFingerprintAsBitVect(m1,2, nBits=2048)
 	return fp 
+
+def fp_array(x):
+	mod, sm = x
+	ret = []
+	for s in sm:
+		ret.append(calcNormalFingerprints(s))
+	return [mod,ret]
+		
 
 def importQuery(name):
 	smis = []
@@ -48,7 +61,6 @@ def importQuery(name):
 			pass
 	return matrix, smis
 	
-#get names of uniprots
 def getUpName():
 	global u_name
 	t_file = open('classes_in_model.txt').read().splitlines()
@@ -57,62 +69,55 @@ def getUpName():
 		t = t.split('\t')
 		u_name[t[1]] = t[0]
 	return
-	
-def introMessage():
-	print '=============================================================================================='
-	print ' Author: Lewis Mervin\n Email:  lhm30@cam.ac.uk\n Supervisor: Dr. A. Bender'
-	print ' Address: Centre For Molecular Informatics, Dept. Chemistry, Lensfield Road, Cambridge CB2 1EW'
-	print '==============================================================================================\n'
-	return 
 	  
 def processtarget(x):
-	filename,fps,inpsmi = x
+	filename,fps = x
 	ret = []
-	pool = modpool[filename]
-	smiles = smilespool[filename]
-	with open(filename, 'rb') as fid:
-		bnb = cPickle.load(fid)
-	#for each wombat compound get nn
 	for i,fp in enumerate(fps):
-		sims = sorted(zip(DataStructs.BulkTanimotoSimilarity(fp,pool),smiles,pool),reverse=True)[:req]
-		for sim in sims:
-			pred = round(bnb.predict_proba([map(int,list(sim[2].ToBitString()))])[:,1],3)
-			ret.append([u_name[filename[7:-4]],filename[7:-4],inpsmi[i],sim[1],str(round(sim[0],3)),str(pred)])
+		line = [u_name[filename[7:-4]],filename[7:-4],smis[i]]
+		res = sorted(zip(DataStructs.BulkTanimotoSimilarity(fp,modfps[filename]),s_dict[filename]),reverse=True)
+		for i in range(req):
+			smi = res[i][1]
+			line.append(smi)
+			sim = round(res[i][0],3)
+			line.append(sim)
+		ret.append(line)
 	return ret
 
 #main
 introMessage()
-output_name = sys.argv[1] + '_out_ad.txt'
-req = int(sys.argv[2])
-of = open(output_name, 'w')
-mods = open('mods.txt').read().splitlines()
+mods = glob.glob('models/*.pkl')
 print 'Total Number of Classes : ' + str(len(mods))
 u_name = dict()
 getUpName()
 fps,smis = importQuery(sys.argv[1])
+req = int(sys.argv[2])
+of = open(sys.argv[1] + '_out_ad_detailed_' + str(req) + '_nn.txt', 'w')
+of.write('Name\tTarget\t' + '\t'.join(map(str,list(np.ravel([['Comp_' + str(i+1),'Sim_' + str(i+1)] for i in range(req)])))) + '\n')
 print 'Total Number of Query Molecules : ' + str(len(fps))
-usr, pw, samples = 'lhm30', 'change', 0
+usr, pw = login()
 conn = pymysql.connect(db='pidgin', user=usr, passwd=pw, host='localhost', port=3306)
-matrix = []
-modpool = dict()
-smilespool = dict()
-for mod in mods:	
-	pool=[]
-	smiles= []
+s_dict = dict()
+print 'Gathering active compounds for all targets'
+for j, mod in enumerate(mods):
 	cur = conn.cursor()
 	cur.execute("SELECT stdsmiles FROM actives WHERE UNIPROT = '"+mod[7:-4]+"';")
-	for row in cur:
-		pool.append(calcNormalFingerprints(row[0]))
-		smiles.append(row[0])
-	modpool[mod] = pool
-	smilespool[mod] = smiles
-#mods = [mod for mod in glob.glob('models/*.pkl')]
-ad_tasks = [[mod,fps,smis] for mod in mods]
+	s_dict[mod] = np.array(cur.fetchall())[:,0]
+
+print 'Calculating fingerprints for all actives'
+modfps = dict()
 pool = Pool(processes=N_cores)  # set up resources
-jobs = pool.imap_unordered(processtarget, ad_tasks)
+jobs = pool.imap_unordered(fp_array, [[mod,smiles] for mod, smiles in s_dict.iteritems()])
 for i, result in enumerate(jobs):
-	for res in result:
-		of.write('\t'.join(map(str,res)) + '\n')
+	modfps[result[0]] = result[1]
 pool.close()
 pool.join()
+
+print 'Calculating near-neighours for all input compounds'
+ad_tasks = [[mod,fps] for mod in sorted(mods)]
+pool = Pool(processes=N_cores)  # set up resources
+jobs = pool.imap(processtarget, ad_tasks)
+for i, result in enumerate(jobs):
+	for r in result:
+		of.write('\t'.join(map(str,r)) + '\n')
 of.close()
